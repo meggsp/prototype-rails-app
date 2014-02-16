260d5d75100d789388aee38f8aef0ded573f8877
your-name <your-email>
2014-02-16 15:00:33 +0000

    Rails with DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205142537) do
+ActiveRecord::Schema.define(version: 20140215211545) do
+
+  create_table "customer_master_ids", force: true do |t|
+    t.string   "company_name"
+    t.string   "primary_contact_name"
+    t.integer  "primary_contact_phone"
+    t.string   "primary_contact_email"
+    t.string   "primary_contact_address_city_state_zip"
+    t.string   "billing_contact_name"
+    t.integer  "billing_contact_phone"
+    t.string   "billing_contact_email"
+    t.string   "billing_contact_address_city_state_zip"
+    t.string   "install_contact_name"
+    t.string   "company_division_sub"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "job_ids", force: true do |t|
+    t.integer  "customer_id"
+    t.string   "customer_PO"
+    t.text     "job_info"
+    t.date     "production_start_date"
+    t.date     "production_due_date"
+    t.date     "delivery_date"
+    t.date     "install_1_date"
+    t.date     "install_2_date"
+    t.text     "production_notes"
+    t.string   "install_location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "prospect_ids", force: true do |t|
+    t.string   "prospect_company"
+    t.string   "prospect_name"
+    t.integer  "prospect_phone"
+    t.string   "prospect_email"
+    t.string   "prospect_address_city_state_zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "quote_ids", force: true do |t|
+    t.integer  "customer_master_id"
+    t.string   "prospect_id"
+    t.text     "project_info"
+    t.decimal  "quote_amount"
+    t.date     "quote_due_date"
+    t.date     "quote_projected_close_date"
+    t.date     "artwork_due_date"
+    t.string   "artwork_proof_link"
+    t.text     "quote_notes"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
